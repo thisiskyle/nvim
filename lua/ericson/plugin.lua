bc220df3e8bdec69b6bab5444e104979894e101e
Kyle Ericson <kyle.ericson2@gmail.com>
2023-08-15 17:03:11 -0400

    some key remap changes

--- lua/ericson/plugin.lua
+++ lua/ericson/plugin.lua
@@ -103,12 +103,12 @@ require("lazy").setup(
                 lsp.on_attach(function(client, bufnr)
                   local opts = {buffer = bufnr, remap = false}
 
-                  vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
-                  vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end, opts)
+                  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
                   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-                  vim.keymap.set("n", "<leader>nd", function() vim.diagnostic.goto_next() end, opts)
-                  vim.keymap.set("n", "<leader>pd", function() vim.diagnostic.goto_prev() end, opts)
+                  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+                  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
                   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
                   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
                   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
