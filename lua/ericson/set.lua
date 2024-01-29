c50c1b4e8f363313b1486f9aa8145a1c45129c6e
Kyle Ericson <kyle.ericson2@gmail.com>
2024-01-29 11:44:57 -0500

    trying out relative number

--- lua/ericson/set.lua
+++ lua/ericson/set.lua
@@ -9,6 +9,7 @@ vim.opt.backup = false
 vim.opt.undofile = true
 vim.opt.termguicolors = true
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab  = true
