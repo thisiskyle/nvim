8f194ef5b8fba5f41cdaa6d683a4db7885922dfa
Kyle Ericson <kyle.ericson@gentex.com>
2024-03-01 12:20:21 -0500

    updates to vibe check that can now save the last used theme and reload it

--- lua/ericson/plugins/vibecheck.lua
+++ lua/ericson/plugins/vibecheck.lua
@@ -3,157 +3,83 @@ return {
         dir = "~/AppData/Local/nvim/pack/my_pack/opt/vibe-check-nvim",
         config = function()
             require('vibe-check-nvim').setup({
-                drab = {
-                    colorscheme = 'drab-day',
-                    background = 'light',
-                    transparent = false,
-                },
+                default = "rose_ghost",
+                use_last = true,
+                vibes = {
+                    drab = {
+                        colorscheme = 'drab-day',
+                        background = 'light',
+                        transparent = false,
+                    },
 
-                drab_night = {
-                    colorscheme = 'drab-night',
-                    background = 'dark',
-                    transparent = false,
-                },
+                    drab_night = {
+                        colorscheme = 'drab-night',
+                        background = 'dark',
+                        transparent = false,
+                    },
 
-                drab_ghost = {
-                    colorscheme = 'drab-night',
-                    background = 'dark',
-                    transparent = true,
-                },
+                    drab_ghost = {
+                        colorscheme = 'drab-night',
+                        background = 'dark',
+                        transparent = true,
+                    },
 
-                cat = {
-                    colorscheme = 'catppuccin',
-                    background = 'dark',
-                    transparent = false,
-                    config = function()
-                        require("catppuccin").setup({
-                            flavour = "frappe",
-                            transparent_background = false,
-                            show_end_of_buffer = true,
-                            styles = {
-                                comments = { "italic" },
-                                conditionals = {},
-                            },
-                            integrations = {
-                                cmp = true,
-                                gitsigns = true,
-                                nvimtree = true,
-                                treesitter = true,
-                            },
-                        })
-                    end,
-                    post = function()
-                        vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
-                    end
-                },
-
-                cat_light = {
-                    colorscheme = 'catppuccin',
-                    background = 'light',
-                    transparent = false,
-                    config = function()
-                        require("catppuccin").setup({
-                            flavour = "latte",
-                            transparent_background = false,
-                            show_end_of_buffer = true,
-                            styles = {
-                                comments = { "italic" },
-                                conditionals = {},
-                            },
-                            integrations = {
-                                cmp = true,
-                                gitsigns = true,
-                                nvimtree = true,
-                                treesitter = true,
-                            },
-                        })
-                    end,
-                    post = function()
-                        vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
-                    end
-                },
-
-                cat_ghost = {
-                    colorscheme = 'catppuccin',
-                    background = 'dark',
-                    transparent = false,
-                    config = function()
-                        require("catppuccin").setup({
-                            flavour = "frappe",
-                            transparent_background = true,
-                            show_end_of_buffer = true,
-                            styles = {
-                                comments = { "italic" },
-                                conditionals = {},
-                            },
-                            integrations = {
-                                cmp = true,
-                                gitsigns = true,
-                                nvimtree = true,
-                                treesitter = true,
-                            },
-                        })
-                    end,
-                    post = function()
-                        vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
-                    end
-                },
-                rose_light = {
-                    colorscheme = 'rose-pine',
-                    background = 'light',
-                    transparent = false,
-                    config = function()
-                        require("rose-pine").setup({
-                            variant = 'dawn',
-                            styles = {
-                                bold = false,
-                                italic = false,
-                                transparency = false
-                            }
-                        })
-                    end,
-                    post = function()
-                        vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
-                    end
-                },
-                rose = {
-                    colorscheme = 'rose-pine',
-                    background = 'dark',
-                    transparent = false,
-                    config = function()
-                        require("rose-pine").setup({
-                            variant = 'moon',
-                            styles = {
-                                bold = false,
-                                italic = false,
-                                transparency = false
-                            }
-                        })
-                    end,
-                    post = function()
-                        vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
-                    end
-                },
-                rose_ghost = {
-                    colorscheme = 'rose-pine',
-                    background = 'dark',
-                    transparent = true,
-                    config = function()
-                        require("rose-pine").setup({
-                            variant = 'moon',
-                            styles = {
-                                bold = false,
-                                italic = false,
-                                transparency = true
-                            }
-                        })
-                    end,
-                    post = function()
-                        vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
-                    end
-                },
+                    rose_light = {
+                        colorscheme = 'rose-pine',
+                        background = 'light',
+                        transparent = false,
+                        config = function()
+                            require("rose-pine").setup({
+                                variant = 'dawn',
+                                styles = {
+                                    bold = false,
+                                    italic = false,
+                                    transparency = false
+                                }
+                            })
+                        end,
+                        post = function()
+                            vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
+                        end
+                    },
+                    rose = {
+                        colorscheme = 'rose-pine',
+                        background = 'dark',
+                        transparent = false,
+                        config = function()
+                            require("rose-pine").setup({
+                                variant = 'moon',
+                                styles = {
+                                    bold = false,
+                                    italic = false,
+                                    transparency = false
+                                }
+                            })
+                        end,
+                        post = function()
+                            vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
+                        end
+                    },
+                    rose_ghost = {
+                        colorscheme = 'rose-pine',
+                        background = 'dark',
+                        transparent = true,
+                        config = function()
+                            require("rose-pine").setup({
+                                variant = 'moon',
+                                styles = {
+                                    bold = false,
+                                    italic = false,
+                                    transparency = true
+                                }
+                            })
+                        end,
+                        post = function()
+                            vim.api.nvim_set_hl(0, 'Todo', { link = 'Comment' })
+                        end
+                    },
+                }
             })
-            require('vibe-check-nvim').set_the_mood("rose")
         end
     },
 }
--- pack/my_pack/opt/vibe-check-nvim/lua/vibe-check-nvim/init.lua
+++ pack/my_pack/opt/vibe-check-nvim/lua/vibe-check-nvim/init.lua
@@ -1,14 +1,26 @@
 
 local M = {}
 
-local vibes = {}
+local configDir = ""
+
+if(vim.loop.os_uname().sysname == "Linux") then
+    configDir = os.getenv("HOME") .. "/.config/nvim/"
+elseif(vim.loop.os_uname().sysname == "Windows") then
+    configDir = os.getenv("userprofile") .. "/AppData/Local/nvim/"
+end
+
+local config = {
+    save_file = configDir .. "/.vibecheck",
+    default = "",
+    use_last = true,
+    vibes = {},
+}
 
 local function set_transparent()
     vim.api.nvim_set_hl(0, "Normal", { bg = 'none' })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = 'none' })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = 'none' })
 end
-
 
 local function apply_theme(_theme)
     if _theme.config then
@@ -27,32 +39,70 @@ local function apply_theme(_theme)
     end
 end
 
-function M.setup(_vibes)
-    vibes = _vibes
+local function use_last()
+
+    local f = io.open(config.save_file, "r")
+
+    if(f == nil) then
+        if(config.default == "") then
+            M.pick_random()
+        else
+            apply_theme(config.vibes[config.default])
+        end
+    else
+        apply_theme(config.vibes[f:read()])
+        f:close()
+    end
+end
+
+local function save(name)
+    local f = assert(io.open(config.save_file, "w"))
+
+    if(f == nil) then
+        print("failed to save: file not found")
+        return
+    end
+    f:write(name)
+    f:close()
+end
+
+function M.setup(_config)
+
+    -- merge the tables
+    for k,v in pairs(_config) do
+        config[k] = v
+    end
+
+    if(config.use_last) then
+        use_last()
+    else
+        apply_theme(config.vibes[config.default])
+    end
 end
 
 --- Sets the theme to the provided name
 ---@param name any
 function M.set_the_mood(name)
-    apply_theme(vibes[name])
+    apply_theme(config.vibes[name])
+    if(config.save_file ~= "") then
+        save(name)
+    end
 end
 
 function M.pick_random()
     -- count number of vibes
     local n = 0
-    for _ in pairs(vibes) do
+    for _ in pairs(config.vibes) do
         n = n + 1
     end
-
     -- generate random number
     math.randomseed(os.time())
     local rand = math.random(1,n)
-
     -- find the key for the number and set theme
     n = 0
-    for k in pairs(vibes) do
+    for k in pairs(config.vibes) do
         if(n == rand) then
-            apply_theme(vibes[k])
+            apply_theme(config.vibes[k])
         end
         n = n + 1
     end
