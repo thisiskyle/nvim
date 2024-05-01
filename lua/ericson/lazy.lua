6e90636a3efc2ef9493d550251ecf0e922ceee00
Kyle Ericson <kyle.ericson@gentex.com>
2024-05-01 12:21:02 -0400

    feat: yep, still dont like using fugitive over CLI

--- lua/ericson/lazy.lua
+++ lua/ericson/lazy.lua
@@ -18,7 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 -- setup plugins
 require("lazy").setup({
     --require('ericson.plugins.nvim-notify'),
-    require('ericson.plugins.fugitive'),
+    --require('ericson.plugins.fugitive'),
     require('ericson.plugins.fidget'),
     require('ericson.plugins.treesitter'),
     require('ericson.plugins.telescope'),
