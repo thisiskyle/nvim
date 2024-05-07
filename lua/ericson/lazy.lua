6a0b787a201433fed54fb414f707f31f7b343a28
Kyle Ericson <kyle.ericson@gentex.com>
2024-05-07 13:30:58 -0400

    ?

--- lua/ericson/lazy.lua
+++ lua/ericson/lazy.lua
@@ -18,8 +18,8 @@ vim.opt.rtp:prepend(lazypath)
 -- setup plugins
 require("lazy").setup({
     --require('ericson.plugins.nvim-notify'),
+    --require('ericson.plugins.fidget'),
     --require('ericson.plugins.fugitive'),
-    require('ericson.plugins.fidget'),
     require('ericson.plugins.treesitter'),
     require('ericson.plugins.telescope'),
     require('ericson.plugins.cmp'),
