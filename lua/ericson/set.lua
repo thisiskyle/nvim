b68c813b12fcf8eb508c7ea12c67b10fa0a3df3b
Kyle Ericson <kyle.ericson2@gmail.com>
2024-05-17 09:49:47 -0400

    feat: quick terminal open

--- lua/ericson/set.lua
+++ lua/ericson/set.lua
@@ -29,3 +29,4 @@ vim.opt.statusline = " "
 
 vim.keymap.set('n', '<leader>tn', "<cmd>set nu!<cr>")
 vim.keymap.set('n', '<leader>tr', "<cmd>set rnu!<cr>")
+vim.keymap.set('n', '<leader>x', ":sp<cr>:term<cr>a")
