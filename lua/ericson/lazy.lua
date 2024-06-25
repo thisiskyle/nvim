ac30fb9f9ca1ae271b37c5ab1bb360afab8fa91f
Kyle Ericson <kyle.ericson2@gmail.com>
2024-06-25 11:24:00 -0400

    feat: removed fugitive

--- lua/ericson/lazy.lua
+++ lua/ericson/lazy.lua
@@ -19,7 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     --require('ericson.plugins.nvim-notify'),
     --require('ericson.plugins.fidget'),
-    require('ericson.plugins.fugitive'),
+    --require('ericson.plugins.fugitive'),
     require('ericson.plugins.treesitter'),
     require('ericson.plugins.telescope'),
     require('ericson.plugins.cmp'),
