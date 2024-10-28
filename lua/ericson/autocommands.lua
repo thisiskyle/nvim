1e79c3a3ef5c38679394081fd5110344c6db19a7
Kyle Ericson <kyle.ericson@gentex.com>
2024-10-28 14:41:01 -0400

    feat: small tweaks

--- lua/ericson/autocommands.lua
+++ lua/ericson/autocommands.lua
@@ -1,8 +1,1 @@
-
-vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-        vim.g.branch_name = require("ericson.utils").set_branch_name()
-    end
-})
-
 vim.cmd([[ au BufRead,BufNewFile *.todo set filetype=todo ]])
