b30621dc568fcff71441a4fe33af1912053c2f19
Kyle Ericson <kyle.ericson2@gmail.com>
2025-02-03 22:36:41 -0500

    some stuff

--- lua/ericson/utils.lua
+++ lua/ericson/utils.lua
@@ -1,29 +1,51 @@
 local M = {}
 
+function M.set_branch_name()
+    local branch = vim.fn.system("git branch --show-current")
+
+    if(string.find(branch, 'fatal')) then
+        branch = ""
+    end
+
+    if branch ~= "" then
+        return branch:gsub('\n', '')
+    else
+        return ""
+    end
+end
+
+function M.git_branch()
+    return vim.g.branch_name
+end
+
 function M.diag_count()
     if vim.fn.has('nvim-0.6') == 1 then
-      -- On nvim 0.6+ use vim.diagnostic to get lsp generated diagnostic count.
-      local diagnostics = vim.diagnostic.get(0)
-      local count = { 0, 0, 0, 0 }
-      for _, diagnostic in ipairs(diagnostics) do
-        if vim.startswith(vim.diagnostic.get_namespace(diagnostic.namespace).name, 'vim.lsp') then
-          count[diagnostic.severity] = count[diagnostic.severity] + 1
+        -- On nvim 0.6+ use vim.diagnostic to get lsp generated diagnostic count.
+        local diagnostics = vim.diagnostic.get(0)
+        local count = { 0, 0, 0, 0 }
+        for _, diagnostic in ipairs(diagnostics) do
+            if vim.startswith(vim.diagnostic.get_namespace(diagnostic.namespace).name, 'vim.lsp') then
+                count[diagnostic.severity] = count[diagnostic.severity] + 1
+            end
         end
-      end
 
-      local return_value = ""
+        local status = ""
 
-      local e = count[vim.diagnostic.severity.ERROR]
-      local w = count[vim.diagnostic.severity.WARN]
-      local i = count[vim.diagnostic.severity.INFO]
-      local h = count[vim.diagnostic.severity.HINT]
+        local e = count[vim.diagnostic.severity.ERROR]
+        local w = count[vim.diagnostic.severity.WARN]
+        local i = count[vim.diagnostic.severity.INFO]
+        local h = count[vim.diagnostic.severity.HINT]
 
-      if(e > 0) then return_value = return_value .. "E:" .. e .. " " end
-      if(w > 0) then return_value = return_value .. "W:" .. w .. " " end
-      if(i > 0) then return_value = return_value .. "I:" .. i .. " " end
-      if(h > 0) then return_value = return_value .. "H:" .. h .. " " end
+        if(e > 0 or w > 0 or i > 0 or h > 0) then
+            status = "diag: "
+        end
 
-      return return_value
+        if(e > 0) then status = status .. "E:" .. e .. " " end
+        if(w > 0) then status = status .. "W:" .. w .. " " end
+        if(i > 0) then status = status .. "I:" .. i .. " " end
+        if(h > 0) then status = status .. "H:" .. h .. " " end
+
+        return status
 
     end
 end
