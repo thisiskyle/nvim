8b004d08ecb0671725fd97195c6bd23f2c3ae9ee
Kyle Ericson <kyle.ericson@gentex.com>
2025-03-12 13:46:06 -0400

    removed the fugitive keymaps

--- lua/ericson/plugins/fugitive.lua
+++ lua/ericson/plugins/fugitive.lua
@@ -1,12 +1,6 @@
 return {
     {
         "tpope/vim-fugitive",
-        lazy = true,
-        keys = {
-            { '<leader>gg', '<cmd>G<cr>', mode = 'n', desc = "fugitive: open" },
-            { '<leader>gl', '<cmd>G adog<cr>', mode = 'n', desc = "fugitive: git adog" },
-            { '<leader>gs', '<cmd>G status<cr>', mode = 'n', desc = "fugitive: git status" },
-        }
-
+        lazy = false,
     }
 }
