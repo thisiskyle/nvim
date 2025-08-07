823dcf90127fa426c53f409cbd119f11b4524bb6
Kyle Ericson <kyle.ericson@gentex.com>
2025-08-07 13:51:33 -0400

    migrate colorschemes to use vim-pack

---
+++ lua/ericson/pack/colorscheme.lua
@@ -0,0 +1,61 @@
+local utils = require("ericson.utils")
+
+return {
+    sources = {
+        "https://github.com/rose-pine/neovim"
+    },
+    setup = function()
+
+        require("rose-pine").setup({
+            variant = "moon",
+            enable = {
+                terminal = true
+            },
+            dim_inactive_windows = false,
+            styles = {
+                bold = false,
+                italic = false,
+                transparency = false,
+            },
+            highlight_groups = {
+                Comment = { italic = true }
+            }
+        })
+
+
+        vim.keymap.set(
+            { 'n' },
+            '<leader>1',
+            function()
+                utils.color_me({ color = "rose-pine" })
+            end,
+            { desc = 'colorscheme: set rose-pine' }
+        )
+        vim.keymap.set(
+            { 'n' },
+            '<leader>2',
+            function()
+                utils.color_me({ color = "quiet" })
+            end,
+            { desc = 'colorscheme: set quiet' }
+        )
+        vim.keymap.set(
+            { 'n' },
+            '<leader>9',
+            function()
+                utils.solid_bg()
+            end,
+            { desc = 'colorscheme: set bg solid' }
+        )
+        vim.keymap.set(
+            { 'n' },
+            '<leader>0',
+            function()
+                utils.transparent_bg()
+            end,
+            { desc = 'colorscheme: set bg tranparent' }
+        )
+
+    end
+}
+
