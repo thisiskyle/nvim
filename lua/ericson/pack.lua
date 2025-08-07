a4047e9e4ccd004fd54684229a4dde73582d15ab
Kyle Ericson <kyle.ericson@gentex.com>
2025-08-07 13:12:09 -0400

    pack will now install everything before running any setups

--- lua/ericson/pack.lua
+++ lua/ericson/pack.lua
@@ -3,12 +3,29 @@ local requirePath = 'ericson.pack.'
 local filePath = vim.fn.stdpath('config') .. '/lua/' .. requirePath:gsub('%.', '/')
 local files = vim.fn.readdir(filePath)
 
+local sources = {}
+local setups = {}
+
+-- sort all the sources and setups into their own arrays
 for _, file in ipairs(files) do
     local pack = require(requirePath .. file:gsub('%.lua$', ''))
     if(pack.enabled ~= false) then
-        vim.pack.add(pack.sources)
+        for _,v in ipairs(pack.sources) do
+            table.insert(sources, v)
+        end
         if(pack.setup) then
-            pack.setup()
+            table.insert(setups, pack.setup)
         end
     end
 end
+
+-- install all sources
+vim.pack.add(sources)
+
+-- run all setups
+for _,v in ipairs(setups) do
+    v()
+end
+
+
+
