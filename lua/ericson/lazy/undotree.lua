7b37e0c6080775ae87f9bab74638a80e178ec342
Kyle Ericson <kyle.ericson@gentex.com>
2025-08-07 15:38:14 -0400

    fix undotree

--- lua/ericson/lazy/undotree.lua
+++ lua/ericson/lazy/undotree.lua
@@ -1,13 +1,19 @@
 return {
     "mbbill/undotree",
     config = function()
-        if(vim.loop.os_uname().sysname == "Windows") then
+        if(string.find(vim.loop.os_uname().sysname, "Windows")) then
             vim.g.undotree_DiffCommand = "FC"
         end
-        vim.g.undotree_WindowLayout = 2
     end,
 
     keys = {
-        { '<leader>u', vim.cmd.UndotreeToggle, mode = 'n' },
+        {
+            '<leader>u',
+            function()
+                vim.cmd.UndotreeToggle()
+                vim.cmd.UndotreeFocus()
+            end,
+            mode = 'n'
+        },
     }
 }
