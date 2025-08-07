e66cf997137d8d8a08df32a0d185f71f0e93f59d
Kyle <none>
2025-08-07 10:34:18 -0400

    trying out vim pack for neovim 0.12
    * migrate typr to pack, remove lazy versions
    * move sleepy keymaps into plugin setup
    * migrate undotree to vim-pack
    * pack will now install everything before running any setups
    * migrate snacks to vim-pack
    * migrate colorschemes to use vim-pack
    * migrate fugitive and treesitter to vim pack
    * refactor colorscheme stuff
    * migrate blink to vim pack
    * fix blink
    * migrate nvim-lspconfig to vim pack

---
+++ lua/personal/pack/sleepy.lua
@@ -0,0 +1,34 @@
+return {
+    enabled = true,
+    sources = {
+        "https://github.com/thisiskyle/sleepy-nvim"
+    },
+    setup = function()
+        require("sleepy").setup({
+            global_after = function()
+                vim.cmd(":%!jq")
+            end,
+        })
+
+        vim.keymap.set(
+            { 'v' },
+            '<leader>ss',
+            ":Sleepy<cr>",
+            { desc = 'sleepy: run' }
+        )
+
+        vim.keymap.set(
+            { 'v' },
+            '<leader>sc',
+            ":SleepyShowCurlCommands<cr>",
+            { desc = 'sleepy: show curl commands' }
+        )
+
+        vim.keymap.set(
+            { 'n' },
+            '<leader>st',
+            ":SleepyTemplate<cr>",
+            { desc = 'sleepy: run' }
+        )
+    end
+}
