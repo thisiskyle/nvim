70eb438122ebb760e32000a446cfe8577dd8a8ac
Kyle <>
2026-04-06 15:41:46 -0400

    updating treesitter config

--- lua/personal/init.lua
+++ lua/personal/init.lua
@@ -1,7 +1,5 @@
 vim.g.mapleader = " "
-
 require("personal.vimpack")
 require("personal.core.keymaps")
 require("personal.core.settings")
 require("personal.core.usercommands")
-require("personal.core.autocommands")
--- lua/personal/vimpack/specs/treesitter.lua
+++ lua/personal/vimpack/specs/treesitter.lua
@@ -1,29 +1,11 @@
-local _opts = {
-    compilers = { "zig", "clang", "gcc" },
-    config = {
-        auto_install = true,
-
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-        },
-
-        indent = {
-            enable = true
-        },
-    }
-}
-
 return {
     enabled = true,
     specs = {
         {
             src = "https://github.com/nvim-treesitter/nvim-treesitter",
-            data = _opts
         },
     },
     setup = function()
-        require('nvim-treesitter.install').compilers = _opts.compilers
-        require('nvim-treesitter.config').setup(_opts.config)
+        require('nvim-treesitter').install({ 'lua', 'typescript', 'bash' })
     end
 }
